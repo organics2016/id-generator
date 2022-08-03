045da6d808f3c4ec04d2df4f9ed673743e0b9349
organics <374564811@qq.com>
2022-08-03 17:16:38 +0800

    update

--- src/test/java/ink/organics/test/FunctionTest.java
+++ src/test/java/ink/organics/test/FunctionTest.java
@@ -15,7 +15,10 @@ public class FunctionTest {
 
     @Test
     public void test() {
+        System.out.println(Long.toBinaryString(-1L));
         System.out.println(~(-1L << 10));
+        System.out.println(Long.toBinaryString(Long.parseLong("111111111111", 2)));
+        System.out.println(Long.parseLong("0111111111111111111111111111111111111111111111111111111111111111", 2));
     }
 
 
